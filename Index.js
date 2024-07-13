7dd61dada85ba8d9a07f92756acc7a84a7a380f3
ferdynandpro <93565401+ferdynandpro@users.noreply.github.com>
2024-07-13 18:54:17 +0700

    Update Index.js
    1

--- Index.js
+++ Index.js
@@ -6,8 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello from Express on Vercel!');
 });
 
-// Additional routes and middleware can be added here
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
