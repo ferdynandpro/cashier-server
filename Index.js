bb4a180d306ec020f13343361a16c0880e3ca4e0
ferdynandpro <93565401+ferdynandpro@users.noreply.github.com>
2024-07-13 18:24:59 +0700

    13/7

--- Index.js
+++ Index.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// MongoDB configuration
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
-const app = express();
-const port = process.env.PORT || 5000;
-
 const uri = "mongodb+srv://mern-product:eJB8vlQTYfr35TCN@cluster0.4etduou.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -17,35 +27,31 @@ const client = new MongoClient(uri, {
 });
 
 const secretKey = 'your_secret_key'; // Ganti dengan secret key Anda yang sebenarnya
-
-// Middleware
-app.use(express.json());
-
-// Set CORS options
-const corsOptions = {
-  origin: 'https://cashier-web-five.vercel.app/login', // Ganti dengan URL frontend Anda
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 async function run() {
   try {
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
+    
 
+    // Create collections
     const buktiPembayaranCollection = client.db("ProductInventoery").collection("bukti-pembayaran");
     const productCollections = client.db("ProductInventoery").collection("products");
     const usersCollection = client.db("ProductInventoery").collection("users");
     const logsCollection = client.db("ProductInventoery").collection("logs");
+    
 
+    // Endpoint to add new payment proof
     app.post("/bukti-pembayaran", async (req, res) => {
       try {
         const { tanggal_pembayaran } = req.body;
         if (!tanggal_pembayaran) {
           return res.status(400).json({ message: "tanggal_pembayaran is required" });
         }
+
         const result = await buktiPembayaranCollection.insertOne({ tanggal_pembayaran, details: [] });
         res.status(201).json({ message: "Payment proof added successfully", result });
       } catch (error) {
@@ -54,6 +60,7 @@ async function run() {
       }
     });
 
+    // Endpoint to add new payment detail
     app.post("/bukti-pembayaran/:buktiId/detail", async (req, res) => {
       try {
         const buktiId = req.params.buktiId;
@@ -61,10 +68,12 @@ async function run() {
         if (!detail) {
           return res.status(400).json({ message: "Detail is required" });
         }
+
         const result = await buktiPembayaranCollection.updateOne(
           { _id: new ObjectId(buktiId) },
           { $push: { details: detail } }
         );
+
         res.status(201).json({ message: "Payment detail added successfully", result });
       } catch (error) {
         console.error("Error adding payment detail:", error);
@@ -72,6 +81,7 @@ async function run() {
       }
     });
 
+    // Endpoint untuk mendapatkan semua bukti pembayaran beserta detailnya
     app.get("/bukti-pembayaran", async (req, res) => {
       try {
         const paymentProofs = await buktiPembayaranCollection.find().toArray();
@@ -82,6 +92,7 @@ async function run() {
       }
     });
 
+    // Endpoint untuk mendapatkan detail pembayaran berdasarkan ID bukti pembayaran
     app.get("/bukti-pembayaran/:buktiId/detail", async (req, res) => {
       try {
         const buktiId = req.params.buktiId;
@@ -96,32 +107,38 @@ async function run() {
       }
     });
 
+    // Insert a product to the database: POST method
     app.post("/upload-product", async (req, res) => {
       const data = req.body;
       const result = await productCollections.insertOne(data);
       res.send(result);
     });
 
+    // Get all products from database
     app.get("/products", async (req, res) => {
       const products = productCollections.find();
       const result = await products.toArray();
       res.send(result);
     });
 
+    // Update a product's data
     app.patch("/product/:id", async (req, res) => {
       const id = req.params.id;
       const updateProductData = req.body;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
+
       const updateDoc = {
         $set: {
           ...updateProductData
         }
       };
+      // Update
       const result = await productCollections.updateOne(filter, updateDoc, options);
       res.send(result);
     });
 
+    // Delete a product
     app.delete("/product/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
@@ -129,6 +146,7 @@ async function run() {
       res.send(result);
     });
 
+    // Get a single product by ID
     app.get("/product/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
@@ -136,14 +154,21 @@ async function run() {
       res.send(result);
     });
 
+    // Register a new user
     app.post("/register", async (req, res) => {
       try {
         const { username, password } = req.body;
+
+        // Check if user already exists
         const existingUser = await usersCollection.findOne({ username });
         if (existingUser) {
           return res.status(400).json({ message: "User already exists" });
         }
+
+        // Hash password
         const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Insert new user into database
         const newUser = {
           username,
           password: hashedPassword
@@ -156,18 +181,26 @@ async function run() {
       }
     });
 
+    // Login a user
     app.post("/login", async (req, res) => {
       try {
         const { username, password } = req.body;
+
+        // Check if user exists
         const user = await usersCollection.findOne({ username });
         if (!user) {
           return res.status(400).json({ message: "Invalid username or password" });
         }
+
+        // Check if password is correct
         const isPasswordValid = await bcrypt.compare(password, user.password);
         if (!isPasswordValid) {
           return res.status(400).json({ message: "Invalid username or password" });
         }
+
+        // Generate a token
         const token = jwt.sign({ userId: user._id, username: user.username }, secretKey, { expiresIn: '1h' });
+
         res.status(200).json({ message: "Login successful", token });
       } catch (error) {
         console.error("Error during login:", error);
@@ -175,24 +208,30 @@ async function run() {
       }
     });
 
+    // Middleware to verify token
     const authenticateToken = (req, res, next) => {
       const token = req.headers['authorization'];
+
       if (!token) {
         return res.status(403).json({ message: 'No token provided' });
       }
+
       jwt.verify(token, secretKey, (err, user) => {
         if (err) {
           return res.status(401).json({ message: 'Invalid token' });
         }
+
         req.user = user;
         next();
       });
     };
 
+    // Example protected route
     app.get("/protected-route", authenticateToken, (req, res) => {
       res.status(200).json({ message: "This is a protected route", user: req.user });
     });
 
+    // Add a new endpoint to save logs
     app.post("/logs", async (req, res) => {
       try {
         const logs = req.body;
@@ -204,6 +243,7 @@ async function run() {
       }
     });
 
+    // Add a new endpoint to get all logs
     app.get("/logs", async (req, res) => {
       try {
         const logs = await logsCollection.find().toArray();
@@ -213,7 +253,9 @@ async function run() {
         res.status(500).json({ message: "Internal server error" });
       }
     });
+    
 
+    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
